94c8973987b64dbf82d45748d9e84f3f8ccfb5c7
ItzMiracleOwO <miraclecutelemon@gmail.com>
2024-10-01 01:17:27 +0000

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 import localFont from "next/font/local";
 import "./globals.css";
 
@@ -31,6 +32,7 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
